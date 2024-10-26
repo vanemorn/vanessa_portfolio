8f83e45569e62cdaf8d8c7e305edd01b92665678
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-26 11:20:14 +0100

    header

---
+++ src/Pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section id="about" className="section">
+      <h1>About Me</h1>
+      <p>
+        Hello, I'm Vanessa, a passionate Graphic Designer with a deep interest in User Interaction and Experience Design.
+      </p>
+    </section>
+  );
+};
+
+export default About;
