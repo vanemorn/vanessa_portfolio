01cfb84e2da22373194639c5a17a4069132cce08
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-29 22:40:23 +0000

    jkfhdkj

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -2,11 +2,28 @@
 import React from 'react';
 import Header from '../components/Header'; // Asegúrate de que la ruta sea correcta
 import Footer from '../components/Footer'; // Asegúrate de que la ruta sea correcta
+import './Gallery.css'; // Asegúrate de que tienes este archivo CSS
+
 
 const About: React.FC = () => {
   return (
     <>
       <Header />
+
+     {/* Contenedor principal del contenido */}
+     <div className="row">
+        <div className="column-gallery">
+        <a href="#" className="go-back">← Go back</a>
+          <h2 className="column-title-gallery">Gallery</h2>
+
+        </div>
+
+        <div className="column-gallery">
+          <p>testing 2nd column</p>
+
+        </div>
+      </div>
+
       <footer>
         <Footer />
       </footer>
