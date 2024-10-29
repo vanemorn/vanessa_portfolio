020abd0934588ae6271dca8d95e5f3def842577f
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-29 23:05:50 +0000

    add gallery

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -1,10 +1,29 @@
 // src/components/Gallery.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header'; // Asegúrate de que la ruta sea correcta
 import Footer from '../components/Footer'; // Asegúrate de que la ruta sea correcta
+import Lightbox from 'react-image-lightbox'; // Asegúrate de haber instalado esta dependencia
+import 'react-image-lightbox/style.css'; // Importa los estilos de Lightbox
 import './Gallery.css'; // Asegúrate de que tienes este archivo CSS
 
-const About: React.FC = () => {
+// Array de imágenes para la galería
+const images = [
+  'C:/Users/linda/vanessa_portfolio/src/assets/image1.jpg',
+  'C:/Users/linda/vanessa_portfolio/src/assets/image2.jpg',
+  'C:/Users/linda/vanessa_portfolio/src/assets/image3.jpg',
+  'C:/Users/linda/vanessa_portfolio/src/assets/image4.jpg',
+  // Añade más imágenes según sea necesario
+];
+
+const Gallery: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const openLightbox = (index: number) => {
+    setCurrentImageIndex(index);
+    setIsOpen(true);
+  };
+
   return (
     <>
       <Header />
@@ -18,6 +37,35 @@ const About: React.FC = () => {
 
         <div className="column-gallery">
           <p>testing 2nd column</p>
+
+          {/* Galería de imágenes */}
+          <div className="image-gallery">
+            {images.map((image, index) => (
+              <img 
+                key={index} 
+                src={image} 
+                alt={`Gallery Image ${index + 1}`} 
+                className="gallery-image"
+                onClick={() => openLightbox(index)}
+              />
+            ))}
+          </div>
+
+          {/* Lightbox */}
+          {isOpen && (
+            <Lightbox
+              mainSrc={images[currentImageIndex]}
+              nextSrc={images[(currentImageIndex + 1) % images.length]}
+              prevSrc={images[(currentImageIndex + images.length - 1) % images.length]}
+              onCloseRequest={() => setIsOpen(false)}
+              onMovePrevRequest={() =>
+                setCurrentImageIndex((currentImageIndex + images.length - 1) % images.length)
+              }
+              onMoveNextRequest={() =>
+                setCurrentImageIndex((currentImageIndex + 1) % images.length)
+              }
+            />
+          )}
         </div>
       </div>
 
@@ -28,4 +76,4 @@ const About: React.FC = () => {
   );
 };
 
-export default About;
+export default Gallery;
