1374d91ceced7061d9ba66968d180a1517a103e7
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-29 23:10:04 +0000

    jkhfdk

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -2,25 +2,23 @@
 import React, { useState } from 'react';
 import Header from '../components/Header'; // Asegúrate de que la ruta sea correcta
 import Footer from '../components/Footer'; // Asegúrate de que la ruta sea correcta
-import Lightbox from 'react-image-lightbox'; // Asegúrate de haber instalado esta dependencia
-import 'react-image-lightbox/style.css'; // Importa los estilos de Lightbox
 import './Gallery.css'; // Asegúrate de que tienes este archivo CSS
 
 // Array de imágenes para la galería
 const images = [
   'C:/Users/linda/vanessa_portfolio/src/assets/image1.jpg',
   'C:/Users/linda/vanessa_portfolio/src/assets/image2.jpg',
-  'C:/Users/linda/vanessa_portfolio/src/assets/image3.jpg',
+  '/C:/Users/linda/vanessa_portfolio/src/assets/image3.jpg',
   'C:/Users/linda/vanessa_portfolio/src/assets/image4.jpg',
   // Añade más imágenes según sea necesario
 ];
 
 const Gallery: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImage, setCurrentImage] = useState('');
 
-  const openLightbox = (index: number) => {
-    setCurrentImageIndex(index);
+  const openImage = (image: string) => {
+    setCurrentImage(image);
     setIsOpen(true);
   };
 
@@ -46,28 +44,19 @@ const Gallery: React.FC = () => {
                 src={image} 
                 alt={`Gallery Image ${index + 1}`} 
                 className="gallery-image"
-                onClick={() => openLightbox(index)}
+                onClick={() => openImage(image)} // Al hacer clic se abre la imagen en overlay
               />
             ))}
           </div>
-
-          {/* Lightbox */}
-          {isOpen && (
-            <Lightbox
-              mainSrc={images[currentImageIndex]}
-              nextSrc={images[(currentImageIndex + 1) % images.length]}
-              prevSrc={images[(currentImageIndex + images.length - 1) % images.length]}
-              onCloseRequest={() => setIsOpen(false)}
-              onMovePrevRequest={() =>
-                setCurrentImageIndex((currentImageIndex + images.length - 1) % images.length)
-              }
-              onMoveNextRequest={() =>
-                setCurrentImageIndex((currentImageIndex + 1) % images.length)
-              }
-            />
-          )}
         </div>
       </div>
+
+      {/* Overlay para mostrar la imagen seleccionada */}
+      {isOpen && (
+        <div className="overlay" onClick={() => setIsOpen(false)}>
+          <img src={currentImage} alt="Expanded view" className="expanded-image" />
+        </div>
+      )}
 
       <footer>
         <Footer />
