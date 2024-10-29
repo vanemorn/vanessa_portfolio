0b5f74f3828cd389caebaf6647005cc86fa124de
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-29 08:29:27 +0000

    añadir footer

---
+++ src/Pages/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'C:/Users/linda/vanessa_portfolio/src/Pages/Footer.css'; // Asegúrate de que la ruta al CSS sea correcta
+import logo from '../assets/logo-blanco.svg'; // Ruta al logo
+import emailIcon from '../assets/email-icon.svg'; // Ruta al ícono de email
+import phoneIcon from '../assets/phone-icon.svg'; // Ruta al ícono de teléfono
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <img src={logo} alt="Logo" className="footer-logo" />
+          <span className="footer-name">Vanessa Moreno</span>
+        </div>
+        <div className="footer-right">
+          <div className="footer-item">
+            <img src={emailIcon} alt="Email Icon" className="footer-icon" />
+            <span className="footer-text">vanessamorenoperez55@gmail.com</span>
+          </div>
+          <div className="footer-item">
+            <img src={phoneIcon} alt="Phone Icon" className="footer-icon" />
+            <span className="footer-text">+353 89 945 1920</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
