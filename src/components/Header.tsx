72c18038815c470a5a13f8ed05bd695f794e489b
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-30 12:16:37 +0000

    jkhfdj

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'C:/Users/linda/vanessa_portfolio/src/components/Header.css';
 import logo from 'C:/Users/linda/vanessa_portfolio/src/assets/logo-header.svg';
 import { Link } from 'react-router-dom';
+import cvFile from './public/CV_VANESSA-MORENO.pdf'
 
 
 const Header: React.FC = () => {
@@ -23,7 +24,7 @@ const Header: React.FC = () => {
       </nav>
 
       <div className="download-button">
-        <a href="#download" className="btn">Download CV</a>
+        <a href={cvFile} download="CV_VANESSA-MORENO.pdf" className="btn">Download CV</a>
       </div>
     </header>
   );
