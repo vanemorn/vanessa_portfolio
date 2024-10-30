233d02b6271aba5bdc9a3a07262de03e951fb602
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-10-30 21:33:53 +0000

    jfhdjk

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -23,41 +23,93 @@ const Blog: React.FC = () => {
             <span className="post-read-time">Reading time: 8 min</span>
           </div>
           <p className="post-content">
-            This photography series includes three different images: a professional headshot, an aesthetic portrait, and a photo that shows my personal passion. Each image is designed to share a specific message or mood, highlighting different aspects of portrait photography and thematic expression.
+            This photography series includes three different images: a professional headshot, an aesthetic portrait, 
+            and a photo that shows my personal passion. Each image is designed to share a specific message or mood, 
+            highlighting different aspects of portrait photography and thematic expression.
+          </p>
 
-            To develop each idea, I started with a brainstorming session to get my thoughts in line with the project brief. I listed possible themes and visual elements that would help to communicate the message I wanted. After picking the key concepts, I sketched out each idea, thinking about important details like location, lighting, camera angles, clothes and props. I also looked for visual references and created mood boards to clarify the overall direction for each shot. All the photos were edited in Adobe Photoshop.
+          <p className="post-content">
+            To develop each idea, I started with a brainstorming session to get my thoughts in line with the project brief. 
+            I listed possible themes and visual elements that would help to communicate the message I wanted. After picking 
+            the key concepts, I sketched out each idea, thinking about important details like location, lighting, camera 
+            angles, clothes and props. I also looked for visual references and created mood boards to clarify the overall
+             direction for each shot. All the photos were edited in Adobe Photoshop.
           </p>
           
           <h3 className= "post-subtitle">Business-Type Headshot</h3>
           <img className= "post-image" src={businessHeadshot} alt="Business Headshot"/>
           <p className="post-content">
-            For the business headshot, I wanted to create an image that balances professionalism with approachability. I chose soft frontal lighting to keep the exposure even across my face and avoid harsh shadows, giving a clean and polished look. I went with a plain white background, which I planned to replace later with a softly blurred office background, adding context without distracting from the subject.
+            For the business headshot, I wanted to create an image that balances professionalism with approachability. 
+            I chose soft frontal lighting to keep the exposure even across my face and avoid harsh shadows, giving a 
+            clean and polished look. I went with a plain white background, which I planned to replace later with a 
+            softly blurred office background, adding context without distracting from the subject.
+          </p>
 
-            In terms of framing, I opted for a close-up shot, focusing on the upper part of my torso and my face to draw attention to my expression. Unlike other portraits where I might not wear my glasses, I decided to wear them here to project a more intellectual appearance, as glasses are often associated with reading and screen work. To strike a balance between formality and friendliness, I tilted my head slightly to the left, breaking the symmetry to avoid a stiff look. I also wore a simple headband to keep my hair back, ensuring my face was clear and focused, along with a turtleneck blouse that added a touch of professionalism while maintaining a modern feel.
+          <p className="post-content">
+            In terms of framing, I opted for a close-up shot, focusing on the upper part of my torso and my face to 
+            draw attention to my expression. Unlike other portraits where I might not wear my glasses, I decided to 
+            wear them here to project a more intellectual appearance, as glasses are often associated with reading 
+            and screen work. To strike a balance between formality and friendliness, I tilted my head slightly to the 
+            left, breaking the symmetry to avoid a stiff look. I also wore a simple headband to keep my hair back, 
+            ensuring my face was clear and focused, along with a turtleneck blouse that added a touch of professionalism 
+            while maintaining a modern feel.
+          </p>
 
-            For facial expressions, I tried out a few options: a big smile, a soft smile without showing my teeth and a neutral look. After checking them out, I chose the full smile for its friendly quality. Including a warm smile gives the image a sense of reliability, which is boosted by the lighting and wardrobe choices.
+          <p className="post-content">
+            For facial expressions, I tried out a few options: a big smile, a soft smile without showing my teeth and 
+            a neutral look. After checking them out, I chose the full smile for its friendly quality. Including a warm 
+            smile gives the image a sense of reliability, which is boosted by the lighting and wardrobe choices.
           </p>
 
           <h3 className= "post-subtitle">Aesthetic Shot</h3>
           <img className= "post-image" src={aestheticShot} alt="Aesthetic Shot" />
           <p className="post-content">
-            For the aesthetic shot, I drew inspiration from cinematic photography that features deep shadows and rich colours, aiming to create a moody and thoughtful atmosphere. I set up the composition with a centered point of interest: a window right in the middle, with me sitting on the window ledge in a relaxed and introspective pose looking at the outside. This setup was meant to symbolize a contrast between inner darkness and the colourful world outside, serving as a reminder of hope in tough times.
+            For the aesthetic shot, I drew inspiration from cinematic photography that features deep shadows and rich 
+            colours, aiming to create a moody and thoughtful atmosphere. I set up the composition with a centered 
+            point of interest: a window right in the middle, with me sitting on the window ledge in a relaxed and 
+            introspective pose looking at the outside. This setup was meant to symbolize a contrast between inner 
+            darkness and the colourful world outside, serving as a reminder of hope in tough times.
+          </p>
 
-            To achieve the right atmosphere, I relied entirely on natural light, avoiding artificial sources to enhance the room's darkness. By positioning the camera to focus on the window, I let the outside light shape the composition, creating a strong contrast between the bright window and the shadowy interior. This lighting choice highlighted my silhouette against the brighter outdoor scene.
+          <p className="post-content">
+            To achieve the right atmosphere, I relied entirely on natural light, avoiding artificial sources to 
+            enhance the room's darkness. By positioning the camera to focus on the window, I let the outside light 
+            shape the composition, creating a strong contrast between the bright window and the shadowy interior. 
+            This lighting choice highlighted my silhouette against the brighter outdoor scene.
+          </p>
 
-            For my outfit, I went with an all-black look to keep the focus on the light and mood without adding any distractions. In post-production, I made a few adjustments to enhance the atmosphere of the image. I boosted the greens visible through the window and subtly increased the contrast to sharpen the silhouette and enrich the tonal range, allowing the main elements of the composition to stand out.
-
-            This shot combines careful lighting, composition, and colour tweaks to create a striking image that tells an emotional story, balancing darkness with a glimpse of light and colour beyond.
+          <p className="post-content">
+            For my outfit, I went with an all-black look to keep the focus on the light and mood without adding any 
+            distractions. In post-production, I made a few adjustments to enhance the atmosphere of the image. I boosted 
+            the greens visible through the window and subtly increased the contrast to sharpen the silhouette and enrich 
+            the tonal range, allowing the main elements of the composition to stand out.
           </p>
 
           <h3 className= "post-subtitle">Passion Shot</h3>
           <img className= "post-image" src={passionShot} alt="Passion Shot" />
           <p className="post-content">
-            For the passion shot, I wanted to capture my favourite activity since moving to Ireland: traveling and exploring new places. To visually share this topic, I carefully arranged a composition with key items related to trip planning. These included a map with some locations circled, a pen, a Pinterest board full of ideas, a tourist brochure with potential destinations and some decorative elements like a small plant and a cup of coffee. This setup was meant to evoke the feeling of getting ready for an adventure.
+            For the passion shot, I wanted to capture my favourite activity since moving to Ireland: traveling and 
+            exploring new places. To visually share this topic, I carefully arranged a composition with key items 
+            related to trip planning. These included a map with some locations circled, a pen, a Pinterest board 
+            full of ideas, a tourist brochure with potential destinations and some decorative elements like a small 
+            plant and a cup of coffee. This setup was meant to evoke the feeling of getting ready for an adventure.
+          </p>
 
-            To create a balanced and visually interesting composition, I arranged these items within the frame, making sure each element contributed to the overall story without overcrowding the space. I had to ensure that details, especially the circled spots on the map, were clear for the viewer. This took several tries to position the objects and adjust the camera angle for the best shot. The goal was to keep things organized while creating a casual, inviting atmosphere that reflects the excitement of planning a trip.
+          <p className="post-content">
+            To create a balanced and visually interesting composition, I arranged these items within the frame, 
+            making sure each element contributed to the overall story without overcrowding the space. I had to 
+            ensure that details, especially the circled spots on the map, were clear for the viewer. This took 
+            several tries to position the objects and adjust the camera angle for the best shot. The goal was to 
+            keep things organized while creating a casual, inviting atmosphere that reflects the excitement of 
+            planning a trip.
+          </p>
 
-            The background for this shot was my desk, which is originally brown and did not fit the colour palette and look I was looking for. To fix this, I used Photoshop to edit the image by selecting the table and adjusting its colour, brightness and contrast, turning it into a white surface. This change made the composition pop, allowing the travel-related items to stand out and creating a cohesive visual experience that matched the theme of exploration and creativity.
+          <p className="post-content">
+            The background for this shot was my desk, which is originally brown and did not fit the colour 
+            palette and look I was looking for. To fix this, I used Photoshop to edit the image by selecting 
+            the table and adjusting its colour, brightness and contrast, turning it into a white surface. This 
+            change made the composition pop, allowing the travel-related items to stand out and creating a 
+            cohesive visual experience that matched the theme of exploration and creativity.
           </p>
         </div>
 
