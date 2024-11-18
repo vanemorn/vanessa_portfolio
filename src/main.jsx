7f2b2a57fc7e6e84061fed121f69a93dc753c1ec
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-18 10:22:49 +0000

    jkfhdjk

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App from './App.tsx';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 
