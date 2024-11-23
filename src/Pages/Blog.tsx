ff20457294102cd329a4427aa0caa582b5d55359
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-23 18:01:55 +0000

    kjhfjdk

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/Header'; // Importing Header
 import Footer from '../components/Footer'; // Importing Footer
 import './Blog.css'; // CSS File
+//import PostsList from '../features/posts/PostsList';
 
 // Images of the blog entry
 import image0 from '../assets/blog-entry-2/0.png';
@@ -20,34 +21,14 @@ import aestheticShot from '../assets/aesthetic-shot.jpg';
 import passionShot from '../assets/passion-shot.jpg';
 
 const Blog: React.FC = () => {
-  const [lightboxOpen, setLightboxOpen] = useState(false);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const openLightbox = (index: number) => {
-    setCurrentImageIndex(index);
-    setLightboxOpen(true);
-  };
-
-  const closeLightbox = () => {
-    setLightboxOpen(false);
-  };
-
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 10); // Loop through the images
-  };
-
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + 10) % 10); // Loop through the images
-  };
-
   return (
     <>
       <Header />
       
-      <div className="container"> {/* Blog container */}
-        <h1 className="blog-title">Blog</h1> {/* Title of the page */}
+      <div className="container"> {/*Blog container */}
+        <h1 className="blog-title">Blog</h1> {/*Title of the page*/}
         
-        {/* First entry */}
+        {/*First entry*/}
         <div className="post">
           <h2 className="post-title">Photography Series Analysis</h2>
           <div className="post-meta">
@@ -135,7 +116,7 @@ const Blog: React.FC = () => {
           </p>
         </div>
 
-        {/* Second entry with gallery */}
+        {/*Second entry*/}
         <div className="post">
           <h2 className="post-title">Contribution to video assignment: Drink for Thought</h2>
           <div className="post-meta">
@@ -143,42 +124,76 @@ const Blog: React.FC = () => {
             <span className="post-read-time">Reading time: 8 min</span>
           </div>
           
-          <div className="post-gallery">
-          </div>
+          <img className="post-image" src={image0} alt="after-effects"/>
+          <img className="post-image" src={image1} alt="acting role"/>
           <p className="post-content">
-            During the last couple of weeks, I worked on a group video assignment titled “Drink for Thought”, 
-            a short film for the Sligo university student film festival. I was asked to act in the film, which 
-            was to be themed around a “dialogue-based film”. My acting role was that of a young professional 
-            talking with an older mentor, exploring the relationship between them through conversation.
+          During the production of the video Drink for Thought, I played as the Manager for the meeting scenes and in 
+          production I took on the role of editor. My responsibilities included reviewing and enhancing the quality 
+          of the footage by balancing brightness, colours, contrast, etc. Combining all the shots and incorporating 
+          special effects and music. 
           </p>
           <p className="post-content">
-            The film was shot in a very short time frame, with locations near our university campus. The whole 
-            process from filming to editing took place over a weekend, and it was incredibly satisfying to see 
-            our efforts come together in a finished product. The story was about mentorship, trust, and life 
-            choices, and I worked closely with the director to establish my character's emotional tone for the 
-            scene. It was a challenging role, but I think it helped me grow as an actor and storyteller.
+          For the editing process, I used Adobe After Effects. Once all the footage was recorded, the team uploaded 
+          the clips to a shared folder, where I retrieved them. I began by cutting and arranging the footage into a 
+          cohesive timeline in After Effects, ensuring the final video would not exceed two minutes.  
+          </p>
+
+          <h3 className="post-subtitle">Visual Adjustments and Editing </h3>
+          <img className="post-image" src={image2} alt="scene1-comparison"/>
+          <img className="post-image" src={image3} alt="scene2-comparison"/>
+          <img className="post-image" src={image4} alt="scene3-comparison"/>
+          <img className="post-image" src={image5} alt="sequence-comparison"/>
+          <p className="post-content">
+          After organizing the footage, I focused on unifying brightness, colours and contrast, since some of the 
+          scenes were filmed from different angles and different illumination. I mainly used the Lumetri Color 
+          tool in After Effects to achieve a consistent look throughout the video
+          </p>
+
+          <h3 className="post-subtitle">Special Effects and Compositions</h3>
+          <img className="post-image" src={image6} alt="product-composition" />
+          <img className="post-image" src={image7} alt="idea-icon" />
+          <img className="post-image" src={image8} alt="credits" />
+          <p className="post-content">
+          Next, I added special effects to enhance the storytelling. Firstly, in the final shot of the apple juice, 
+          I replaced the background with a graphic composition created in Adobe Photoshop.  Secondly, I animated a 
+          light bulb that appears when the designer conceives a new packaging idea. I also created fade-in and 
+          fade-out compositions in Adobe Illustrator and incorporated a translucent screen transition with the "two weeks later”. 
+          </p>
+
+          <h3 className="post-subtitle">Audio Design</h3>
+          <p className="post-content">
+          Since all scenes were silent, I selected music and sound effects to complement the video. Using Bensound.com and 
+          Freesound.org, I matched music to the three narrative sections: failure, change, and success, ensuring the mood aligned 
+          with the emotions of the protagonist. For sound effects, I synchronized the "idea" sound and the apple-biting sound, 
+          both downloaded from Freesound, with the composed shoots. 
+          </p>
+
+          <h3 className="post-subtitle">Export and Final Details</h3>
+          <img className="post-image" src={image9} alt="export-configuration" />
+          <p className="post-content">
+          Finally, I exported the final video using Adobe Media Encoder. To meet the 25 MB size limit without sacrificing quality, 
+          I adjusted the bitrate encoding to VBR 2-pass with bitrates set between 1.57 and 5.37. This optimization ensured the 
+          video maintained high quality while adhering to file size constraints. 
+          </p>
+
+          <h3 className="post-subtitle">Outcome Reflection</h3>
+          <p className="post-content">
+          This project allowed me to manage post-production editing and creative enhancements. My role was instrumental in 
+          unifying the team's efforts into a cohesive final product that communicated our idea. I particularly learnt a lot 
+          about video compression because of the file size limits, it required a lot of research and trial-error. 
           </p>
           <p className="post-content">
-            My role in this project also included assisting with post-production tasks. The film’s editing involved 
-            a lot of sound work, adding layers of ambient sound and dialogue to bring the film’s world to life. 
-            I worked on After Effects to help integrate various visual elements and enhance the impact of 
-            the film’s themes.
+          I feel satisfied and proud with how my contributions brought the project all together and how it aligned with 
+          the team’s collective vision.  
           </p>
+          
         </div>
 
-        {lightboxOpen && (
-          <div className="lightbox">
-            <div className="lightbox-content">
-              <button className="close-btn" onClick={closeLightbox}>Close</button>
-              <button className="prev-btn" onClick={prevImage}>Previous</button>
-              <img src={[image0, image1, image2, image3, image4, image5, image6, image7, image8, image9][currentImageIndex]} alt="Lightbox" />
-              <button className="next-btn" onClick={nextImage}>Next</button>
-            </div>
-          </div>
-        )}
       </div>
 
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };
