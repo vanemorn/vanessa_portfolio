3dcc7b72447449bd7389cdc6e7ce958c9cad3fdf
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-23 17:17:20 +0000

    Jkhfjkd

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -130,7 +130,6 @@ const Blog: React.FC = () => {
             <span className="post-read-time">Reading time: 8 min</span>
           </div>
           
-          <img className= "post-image" src={image0} alt="after-effects"/>
           <img className= "post-image" src={image1} alt="acting role"/>
           <p className="post-content">
           During the production of the video Drink for Thought, I played as the Manager for the meeting scenes and in 
@@ -143,6 +142,8 @@ const Blog: React.FC = () => {
           the clips to a shared folder, where I retrieved them. I began by cutting and arranging the footage into a 
           cohesive timeline in After Effects, ensuring the final video would not exceed two minutes.  
           </p>
+          <img className= "post-image" src={image0} alt="after-effects"/>
+
 
           <h3 className= "post-subtitle">Visual Adjustments and Editing </h3>
           <img className= "post-image" src={image2} alt="scene1-comparison"/>
