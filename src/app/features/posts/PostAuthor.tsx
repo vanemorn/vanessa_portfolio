63bdcfc2e85580369c9f88059cc20d125844c7a4
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-25 21:23:08 +0000

    jkhfjd

--- src/app/features/posts/PostAuthor.tsx
+++ src/app/features/posts/PostAuthor.tsx
@@ -2,13 +2,15 @@ import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 
 interface PostAuthorProps {
-  userId: string; 
+  userId?: string; 
 }
 
 const PostAuthor: React.FC<PostAuthorProps> = ({ userId }) => {
     const users = useSelector(selectAllUsers);
 
-    const author = Array.isArray(users) ? users.find(user => user.id === userId) : null;
+    const author = userId ? 
+      Array.isArray(users) ? users.find(user => user.id === userId) : null 
+      : null;
 
     return <span>by {author ? author.name : 'Unknown author'}</span>;
 };
