185cb8d62b401092c460e0da0eaa8fcc39c1b262
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-26 22:37:30 +0000

    jnfjd

--- src/app/features/posts/store.tsx
+++ src/app/features/posts/store.tsx
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from '../users/usersSlice';
 import postsReducer from './postsSlice';
 
 export const store = configureStore ({
   reducer: {
     posts: postsReducer,
-    users: usersReducer
   }
 });
 
