e53e478b21a90c1a2722114043562b869cacb999
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 18:37:53 +0000

    improving header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'C:/Users/linda/vanessa_portfolio/src/components/Header.css'; // CSS File
 import logo from 'C:/Users/linda/vanessa_portfolio/src/assets/logo-header.svg'; // Logo
 import { Link } from 'react-router-dom'; // Routing the pages
@@ -7,10 +7,30 @@ import ThemeSwitcher from '../store/ThemeSwitcher'; // Import the ThemeSwitcher
 
 const Header: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null); // To detect clicks outside the menu
 
+  // Toggle the dropdown menu visibility
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
+  // Close the dropdown if clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // Type assertion to ensure event.target is an HTMLElement
+      const target = event.target as HTMLElement;
+
+      // Check if the target is not within the menu or the hamburger button
+      if (menuRef.current && !menuRef.current.contains(target) && !target.classList.contains('dropdown-toggle')) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="header-container">
@@ -26,7 +46,7 @@ const Header: React.FC = () => {
       </button>
 
       {/* Main Menu */}
-      <nav className={`menu ${isDropdownOpen ? 'show' : ''}`}>
+      <nav className={`menu ${isDropdownOpen ? 'show' : ''}`} ref={menuRef}>
         <Link to="/About" onClick={() => setIsDropdownOpen(false)}>About</Link>
         <Link to="/Projects" onClick={() => setIsDropdownOpen(false)}>Projects</Link>
         <Link to="/Contact" onClick={() => setIsDropdownOpen(false)}>Contact</Link>
