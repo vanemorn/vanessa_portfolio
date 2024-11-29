206258d21b703cc37174d31764ab417fa828e70c
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 15:21:14 +0000

    change header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// src/App.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from './store';
-import ThemeSwitcher from './store/ThemeSwitcher';
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Importing pages
 import Home from './Pages/Home';
@@ -16,22 +14,18 @@ import Blog from './Pages/Blog';
 import Videogallery from './Pages/Videogallery';
 import Post from './components/Post';
 
+// Import Header
+import Header from './components/Header';
+
 import './App.css'; // CSS File
 
 const App: React.FC = () => {
-
   const theme = useSelector((state: RootState) => state.theme.theme);
 
   // On theme change, update the HTML body class to reflect the theme
   useEffect(() => {
     document.body.className = theme; // Set body class to 'light' or 'dark'
   }, [theme]);
-
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
 
   return (
     <Router basename="/vanessa_portfolio"> {/* Project root */}
@@ -40,26 +34,8 @@ const App: React.FC = () => {
       </div>
 
       <div className="app-container">
-        {/* Navigation Bar */}
-        <nav className="navbar">
-          <div className="nav-logo">
-            <Link to="/">Vanessa's Portfolio</Link>
-          </div>
-          
-          <button className="dropdown-toggle" onClick={toggleDropdown}>
-            ☰ {/* Hamburger Menu Icon */}
-          </button>
-          <ul className={`nav-links ${isDropdownOpen ? 'show' : ''}`}>
-            <li><Link to="/" onClick={() => setIsDropdownOpen(false)}>Home</Link></li>
-            <li><Link to="/about" onClick={() => setIsDropdownOpen(false)}>About</Link></li>
-            <li><Link to="/projects" onClick={() => setIsDropdownOpen(false)}>Projects</Link></li>
-            <li><Link to="/contact" onClick={() => setIsDropdownOpen(false)}>Contact</Link></li>
-            <li><Link to="/gallery" onClick={() => setIsDropdownOpen(false)}>Gallery</Link></li>
-            <li><Link to="/blog" onClick={() => setIsDropdownOpen(false)}>Blog</Link></li>
-            <li><Link to="/videogallery" onClick={() => setIsDropdownOpen(false)}>Video Gallery</Link></li>
-          </ul>
-          <ThemeSwitcher />
-        </nav>
+        {/* Include Header Component */}
+        <Header />
 
         {/* Routes */}
         <Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import 'C:/Users/linda/vanessa_portfolio/src/components/Header.css'; // CSS File
-import logo from 'C:/Users/linda/vanessa_portfolio/src/assets/logo-header.svg'; // Logo
+import './Header.css';
 import { Link } from 'react-router-dom'; // Routing the pages
-import cvFile from 'C:/Users/linda/vanessa_portfolio/public/CV_VANESSA-MORENO.pdf'// Downloadable PDF of my CV
+import logo from 'C:/Users/linda/vanessa_portfolio/src/assets/logo-header.svg'; // Logo
+import cvFile from 'C:/Users/linda/vanessa_portfolio/public/CV_VANESSA-MORENO.pdf'; // Downloadable PDF of my CV
+import 'C:/Users/linda/vanessa_portfolio/src/components/Header.css'; // CSS File
 
 
 const Header: React.FC = () => {
@@ -13,15 +14,15 @@ const Header: React.FC = () => {
           <img src={logo} alt="logo" className="logo-img" /> 
         </Link>
       </div>
-      
+
       {/* Links to the other pages through the menu */}
       <nav className="menu">
-        <Link to="/About">About</Link>
-        <Link to="/Projects">Projects</Link>
-        <Link to="/Contact">Contact</Link>
-        <Link to="/Blog">Blog</Link>
-        <Link to="/Gallery">Gallery</Link>
-        <Link to="/Videogallery">VideoGallery</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/videogallery">Video Gallery</Link>
       </nav>
 
       {/* The button to download my CV */}
