4b38e0ef51c17254bbc06783ac05c0124450cd16
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 01:13:07 +0000

    add dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 // Importing pages
 import Home from './Pages/Home';
@@ -13,21 +13,44 @@ import Videogallery from './Pages/Videogallery';
 import './App.css'; // CSS File
 
 const App: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   return (
     <Router basename="/vanessa_portfolio"> {/* Project root */}
       <div className="app-container">
-          <Routes>
-            {/* Top-level routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/videogallery" element={<Videogallery />} />
-          </Routes>
-        
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <div className="nav-logo">
+            <Link to="/">Vanessa's Portfolio</Link>
+          </div>
+          <button className="dropdown-toggle" onClick={toggleDropdown}>
+            ☰ {/* Hamburger Menu Icon */}
+          </button>
+          <ul className={`nav-links ${isDropdownOpen ? 'show' : ''}`}>
+            <li><Link to="/" onClick={() => setIsDropdownOpen(false)}>Home</Link></li>
+            <li><Link to="/about" onClick={() => setIsDropdownOpen(false)}>About</Link></li>
+            <li><Link to="/projects" onClick={() => setIsDropdownOpen(false)}>Projects</Link></li>
+            <li><Link to="/contact" onClick={() => setIsDropdownOpen(false)}>Contact</Link></li>
+            <li><Link to="/gallery" onClick={() => setIsDropdownOpen(false)}>Gallery</Link></li>
+            <li><Link to="/blog" onClick={() => setIsDropdownOpen(false)}>Blog</Link></li>
+            <li><Link to="/videogallery" onClick={() => setIsDropdownOpen(false)}>Video Gallery</Link></li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/videogallery" element={<Videogallery />} />
+        </Routes>
       </div>
     </Router>
   );
