0a06775befe5ee5d73dc72876fb03e7b3ee1449a
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 22:59:46 +0000

    projects grid and remove content home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,13 +5,6 @@ import Chatbot from '../components/Chatbot'; // Import the Chatbot component
 import ScrollingBand from '../components/Scrollingband.tsx';
 import ProjectGrid from '../components/ProjectGrid';
 
-
-//Importing images for the featured works section
-import project1 from '../assets/project1.png'; 
-import project2 from '../assets/project2.png'; 
-import project3 from '../assets/project3.png'; 
-import project4 from '../assets/project4.png'; 
-
 // Imports send icon for contact form
 import sendIcon from '../assets/sendicon.png'; 
 
@@ -44,51 +37,6 @@ const Home: React.FC = () => {
 
       <ScrollingBand />
       <ProjectGrid />
-
-
-      {/* Featured works section */}
-      <div className="featured-works-section">
-        <h2 className="featured-works-title" style={{ fontFamily: 'Arial, sans-serif' }}>Featured Works</h2>
-
-        <div className="featured-works">
-          <div className="work-box">
-
-            {/* Project 1 */}
-            <img src={project1} alt="Project 1" className="project-image-home" />
-            <div className="project-label">Project</div>
-            <h3 className="project-title">Ños! Gofio</h3>
-            <p className="project-skills">Logo Design · Packaging · Web Design · Merchandising</p>
-            <a href="#" className="view-more-btn">View More ➔</a>
-          </div>
-
-           {/* Project 2 */}
-          <div className="work-box">
-            <img src={project2} alt="Project 2" className="project-image-home" />
-            <div className="project-label">Project</div>
-            <h3 className="project-title">Cinedfest</h3>
-            <p className="project-skills">Logo Design · Social Media · Poster · Merchandising</p>
-            <a href="#" className="view-more-btn">View More ➔</a>
-          </div>
-
-          {/* Project 3 */}
-          <div className="work-box">
-            <img src={project3} alt="Project 3" className="project-image-home" />
-            <div className="project-label">Project</div>
-            <h3 className="project-title">Cipselas</h3>
-            <p className="project-skills">Layout Design · Visual System · Microtypography</p>
-            <a href="#" className="view-more-btn">View More ➔</a>
-          </div>
-
-          {/* Project 4 */}
-          <div className="work-box">
-            <img src={project4} alt="Project 4" className="project-image-home" />
-            <div className="project-label">Project</div>
-            <h3 className="project-title">Greenport Dashboard</h3>
-            <p className="project-skills">Logo Design · UI Design · Visual System · Diagrams</p>
-            <a href="#" className="view-more-btn">View More ➔</a>
-          </div>
-        </div>
-      </div>
 
       {/* Contact form*/}
       <div className="contact-form-section">
