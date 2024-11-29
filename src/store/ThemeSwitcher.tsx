105f9b2b42f5c4e3f3763391297b8973d2ce12db
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 17:18:01 +0000

    jkfhdjk

--- src/store/ThemeSwitcher.tsx
+++ src/store/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import { toggleTheme } from '../store/themeSlice';
 import { RootState } from '../store';
 import './ThemeSwitcher.css'; // Import CSS for styling
 
-// Import the SVGs as normal image files
+// Import the custom icons (Assuming they are in src/assets folder)
 import moonIcon from '../assets/darkmode.svg'; // Path to the moon icon
 import sunIcon from '../assets/lightmode.svg';   // Path to the sun icon
 
@@ -25,14 +25,14 @@ const ThemeSwitcher: React.FC = () => {
           onChange={handleToggle} 
         />
         <span className="slider"></span>
-
+        
         {/* Display the appropriate icon based on the current theme */}
         <span className="theme-icon">
-          {/* Show the moon icon for dark mode and sun icon for light mode */}
-          <img 
-            src={theme === 'dark' ? moonIcon : sunIcon} 
-            alt={theme === 'dark' ? 'Moon Icon' : 'Sun Icon'} 
-          />
+          {theme === 'dark' ? (
+            <img src={moonIcon} alt="Moon Icon" />
+          ) : (
+            <img src={sunIcon} alt="Sun Icon" />
+          )}
         </span>
       </label>
     </div>
