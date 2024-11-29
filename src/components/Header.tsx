8194507fcd8f3502e07a69ec744eafa8add3ba3f
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-11-29 19:50:09 +0000

    going back

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import 'C:/Users/linda/vanessa_portfolio/src/components/Header.css'; // CSS File
-import logo from 'C:/Users/linda/vanessa_portfolio/public/logo-header-negro.svg'; // Logo
+import logo from 'C:/Users/linda/vanessa_portfolio/src/assets/logo-header.svg'; // Logo
 import { Link } from 'react-router-dom'; // Routing the pages
 import cvFile from 'C:/Users/linda/vanessa_portfolio/public/CV_VANESSA-MORENO.pdf'; // Downloadable PDF of my CV
 import ThemeSwitcher from '../store/ThemeSwitcher'; // Import the ThemeSwitcher
 
 const Header: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null); // To detect clicks outside the menu
 
-  // Toggle the dropdown menu visibility
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
-
-  // Close the dropdown if clicked outside
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      // Type assertion to ensure event.target is an HTMLElement
-      const target = event.target as HTMLElement;
-
-      // Check if the target is not within the menu or the hamburger button
-      if (menuRef.current && !menuRef.current.contains(target) && !target.classList.contains('dropdown-toggle')) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
 
   return (
     <header className="header-container">
@@ -46,7 +26,7 @@ const Header: React.FC = () => {
       </button>
 
       {/* Main Menu */}
-      <nav className={`menu ${isDropdownOpen ? 'show' : ''}`} ref={menuRef}>
+      <nav className={`menu ${isDropdownOpen ? 'show' : ''}`}>
         <Link to="/About" onClick={() => setIsDropdownOpen(false)}>About</Link>
         <Link to="/Projects" onClick={() => setIsDropdownOpen(false)}>Projects</Link>
         <Link to="/Contact" onClick={() => setIsDropdownOpen(false)}>Contact</Link>
