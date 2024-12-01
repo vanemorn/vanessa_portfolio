25bff71e154179f4c49e0eea1586c76f36d33789
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-12-01 14:57:52 +0000

    improving projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,58 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../components/Footer'; // Importing Footer
 import './Projects.css'; //CSS file
 import ProjectGrid from '../components/ProjectGrid';
-
-//Importing icons for Gallery and VideoGallery buttons
-import galleryIcon from '../assets/gallery-icon.svg'; 
-import videogalleryIcon from '../assets/videogallery-icon.svg';
+import Chatbot from '../components/Chatbot'; // Import the Chatbot component
 
 const Projects: React.FC = () => {
   return (
     <>
+
+      {/*The left column */}
+      <div className="projects-container"> 
       
-      <div className="row"> 
-        <div className="column-project"> 
-
-          {/* Left column with introductory text and navigation buttons */}
-
-          <h2 className="column-title-project">Projects</h2>
-          <p className="column-text-project">
-            With a background in Graphic Design, I’ve developed a refined <br />
-            visual sensitivity that, combined with my UX/UI specialization, <br />
-            allows me to create impactful projects across various fields of <br />
-            visual communication. From branding and logo design to web <br />
-            development and interface design, my experience spans <br />
-            multiple aspects of digital and visual storytelling.
+        <div className="projects-text">
+          <h2 className="projects-title">Projects</h2>
+          <p className="projects-description">
+          With a background in Graphic Design, I’ve developed a refined
+          visual sensitivity that, combined with my UX/UI specialization,
+          allows me to create impactful projects across various fields of
+          visual communication. From branding and logo design to web
+          development and interface design, my experience spans
+          multiple aspects of digital and visual storytelling.
           </p>
-          <p className="column-text-project">
-            Feel free to explore individual projects or browse <br />
-            through the galleries for a closer look at my work.
+          <p className="projects-description">
+          Feel free to explore individual projects or browse
+          through the galleries for a closer look at my work.
           </p>
-
-          <button type="button" className="gallery-btn"> {/*Gallery button*/}
-            <Link to="/gallery" className="gallery-link">
-              <span>Gallery</span>
-              <img src={galleryIcon} alt="gallery" className="gallery-icon" />
-            </Link>
-          </button>
-
-          <button type="button" className="videogallery-btn"> {/*VideoGallery button*/}
-            <Link to="/videogallery" className="gallery-link">
-              <span>Video Gallery</span>
-              <img src={videogalleryIcon} alt="video gallery" className="gallery-icon" />
-            </Link>
-          </button>
+          <a href="mailto:vanessamorenoperez55@gmail.com">
+          <button className="gallery-btn">Send email</button>
+          </a>
         </div>
 
-        {/* Right column with projects grid*/}
-
-        <div className="column">
-        <ProjectGrid />
+        {/* The right column */}
+        <div className="projects-container">
+          <ProjectGrid/>
         </div>
       </div>
-
+      <Chatbot />
       <footer>
         <Footer />
       </footer>
