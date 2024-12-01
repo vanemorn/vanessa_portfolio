aec0848737473ec6dd9046fe00eb4a665c9155e4
Vanessa Moreno Pérez <24163988@studentmail.ul.ie>
2024-12-01 15:06:51 +0000

    improving projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,50 +1,40 @@
 import React from 'react';
-import Footer from '../components/Footer'; // Importing Footer
-import './Projects.css'; //CSS file
+import Footer from '../components/Footer';
+import './Projects.css';
 import ProjectGrid from '../components/ProjectGrid';
-import Chatbot from '../components/Chatbot'; // Import the Chatbot component
+import Chatbot from '../components/Chatbot';
 
 const Projects: React.FC = () => {
   return (
     <>
-
-      {/*The left column */}
-      <div className="projects-container"> 
-      
-        <div className= "left-column">
-
-        <div className="projects-text">
-          <h2 className="projects-title">Projects</h2>
-          <p className="projects-description">
-          With a background in Graphic Design, I’ve developed a refined
-          visual sensitivity that, combined with my UX/UI specialization,
-          allows me to create impactful projects across various fields of
-          visual communication. From branding and logo design to web
-          development and interface design, my experience spans
-          multiple aspects of digital and visual storytelling.
-          </p>
-          <p className="projects-description">
-          Feel free to explore individual projects or browse
-          through the galleries for a closer look at my work.
-          </p>
-          <a href="mailto:vanessamorenoperez55@gmail.com">
-          <button className="gallery-btn">Gallery</button>
-          <button className="gallery-btn">VideoGallery</button>
-          </a>
-        </div>
-
-        </div>
-
-        <div className= "right-column">
-
-          <div className="projects-container">
-            <ProjectGrid/>
+      <div className="projects-container">
+        <div className="left-column">
+          <div className="projects-text">
+            <h2 className="projects-title">Projects</h2>
+            <p className="projects-description">
+              With a background in Graphic Design, I’ve developed a refined
+              visual sensitivity that, combined with my UX/UI specialization,
+              allows me to create impactful projects across various fields of
+              visual communication. From branding and logo design to web
+              development and interface design, my experience spans multiple
+              aspects of digital and visual storytelling.
+            </p>
+            <p className="projects-description">
+              Feel free to explore individual projects or browse through the
+              galleries for a closer look at my work.
+            </p>
+            <a href="mailto:vanessamorenoperez55@gmail.com">
+              <button className="gallery-btn">Gallery</button>
+              <button className="gallery-btn">VideoGallery</button>
+            </a>
           </div>
+        </div>
 
-        </div> 
+        <div className="right-column">
+          <ProjectGrid />
+        </div>
       </div>
 
-      
       <Chatbot />
       <footer>
         <Footer />
